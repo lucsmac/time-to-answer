c06fe211de9ac6cd4dcb0f488b981ad4f8d419ec
Lucas Macedo <lucasmdc57@gmail.com>
2021-04-09 07:54:24 -0300

    creating subjects list

--- app/controllers/admins_backoffice/subjects_controller.rb
+++ app/controllers/admins_backoffice/subjects_controller.rb
@@ -12,7 +12,7 @@ class AdminsBackoffice::SubjectsController < AdminsBackofficeController
   def create
     @subject = Subject.new(params_subject)
     if @subject.save()
-      redirect_to admins_backoffice_admins_path, notice: "Administrador cadastrado com sucesso"
+      redirect_to admins_backoffice_subjects_path, notice: "Assunto/Área cadastrado com sucesso"
     else
       render :new
     end
@@ -23,7 +23,7 @@ class AdminsBackoffice::SubjectsController < AdminsBackofficeController
 
   def update
     if @subject.update(params_subject)
-      redirect_to admins_backoffice_admins_path, notice: "Administrador atualizado com sucesso"
+      redirect_to admins_backoffice_subjects_path, notice: "Assunto/Área atualizado com sucesso"
     else
       render :edit
     end
@@ -31,7 +31,7 @@ class AdminsBackoffice::SubjectsController < AdminsBackofficeController
 
   def destroy
     if @subject.destroy
-      redirect_to admins_backoffice_admins_path, notice: "Administrador excluído com sucesso"
+      redirect_to admins_backoffice_subjects_path, notice: "Assunto/Área excluído com sucesso"
     else
       render :index
     end
@@ -40,16 +40,10 @@ class AdminsBackoffice::SubjectsController < AdminsBackofficeController
   private 
     
     def params_subject
-      params.require(:admin).permit(:email, :password, :password_confirmation)
+      params.require(:subject).permit(:description)
     end
 
     def set_subject
       @subject = Subject.find(params[:id])
-    end
-
-    def verify_password
-      if params[:admin][:password].blank? && params[:admin][:confirmation].blank?
-        params[:admin].extract!(:password, :password_confirmation)
-      end
     end
 end
