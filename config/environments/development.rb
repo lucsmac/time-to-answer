3903b50cc0a38facead24d6c5b4398baecf36a3b
Lucas Macedo <lucasmdc57@gmail.com>
2021-04-19 11:53:08 -0300

    implements reset password

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,6 +4,17 @@ Rails.application.configure do
   # Devise config
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  # Mailtrap
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => '7170b3ef2279d4',
+    :password => 'e981aa4838d500',
+    :address => 'smtp.mailtrap.io',
+    :domain => 'smtp.mailtrap.io',
+    :port => '2525',
+    :authentication => :cram_md5
+  }
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
